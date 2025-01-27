4fe4bf59262544c313ac793d1968a069cc9aac21
AmbalaSumithReddy <reddysumith218@gmail.com>
2025-01-27 19:19:17 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# Declaring variables of different data types
+integer_var = 10
+float_var = 3.14
+string_var = "Hello, world!"
+boolean_var = True
+
+# Printing each variable and its type
+print("Integer variable:", integer_var, "Type:", type(integer_var))
+print("Float variable:", float_var, "Type:", type(float_var))
+print("String variable:", string_var, "Type:", type(string_var))
+print("Boolean variable:", boolean_var, "Type:", type(boolean_var))
+
