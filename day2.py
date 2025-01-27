40b49211894a533c433e5a9cd97bf9c928860fbe
AmbalaSumithReddy <reddysumith218@gmail.com>
2025-01-27 19:16:23 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+my_list = [10, 20, 30, 40, 50]
+# Accessing elements based on index
+first_element = my_list[0]  # 10
+third_element = my_list[2]  # 30
+last_element = my_list[-1]  # 50
+
+print(first_element)  # Output: 10
+print(third_element)  # Output: 30
+print(last_element)   # Output: 50
+
+
+# In[3]:
+
+
+my_dict = {'name': 'Alice', 'age': 25, 'city': 'New York', 'occupation': 'Engineer', 'hobby': 'painting'}
+# Accessing elements based on key
+name = my_dict['name']  # 'Alice'
+age = my_dict['age']    # 25
+city = my_dict['city']  # 'New York'
+
+print(name)  # Output: Alice
+print(age)   # Output: 25
+print(city)  # Output: New York
+
